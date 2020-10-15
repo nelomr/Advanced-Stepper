e3b768fa2782bb99725e29deb5272622e69a45f2
Nelo <emartinez@wearemarketing.com>
2020-10-15 19:39:18 +0200

    update vue config for producction

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
           data: `@import "@/assets/styles/_global.scss";`
         }
       }
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Advanced-Stepper/'
+      : '/'
 };
